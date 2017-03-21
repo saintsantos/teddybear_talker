164fe74c36e42e725a1d59421aaf59746250a62a
Michael Drewitt <mikedrewitt@gmail.com>
2017-03-21 18:15:08 -0400

    moving to new react electron folder

---
+++ react-app/app/containers/HomePage.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+
+import Home from '../components/Home';
+import Navbar from '../components/Navbar';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
