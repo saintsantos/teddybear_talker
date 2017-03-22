44998f7af15bb31ae43f01fbc3b6357cb5c9060e
Edwin Santos <saintsantos1341@gmail.com>
2017-03-22 11:50:11 -0400

    Very rough layout of webapp

--- react-app/app/components/EventList/EventList.jsx
+++ react-app/app/components/EventList/EventList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Panel, Button, ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap';
+import { Panel, Button, ListGroup, ListGroupItem, Row, Col, Nav, NavItem, DropdownButton, MenuItem } from 'react-bootstrap';
 import EventElement from '../EventElement/EventElement.jsx';
 import './EventList.css';
 
@@ -7,12 +7,35 @@ export default class EventList extends Component {
     render() {
         return (
             <Panel>
-                <ListGroup>
-                    <EventElement />
-                    <EventElement />
-                    <EventElement />
-                    <EventElement />
-                </ListGroup>
+                <Row>
+                    <Nav bsStyle='tabs' justified activeKey={1}>
+                        <NavItem eventKey={1}>Day View</NavItem>
+                        <NavItem eventKey={2}>Week View</NavItem>
+                    </Nav>
+                </Row>
+                <Row>
+                    <Row>
+                        <DropdownButton bsStyle="default" title="Day">
+                            <MenuItem>Monday</MenuItem>
+                            <MenuItem>Tuesday</MenuItem>
+                            <MenuItem>Wednesday</MenuItem>
+                            <MenuItem>Thursday</MenuItem>
+                            <MenuItem>Friday</MenuItem>
+                            <MenuItem>Saturday</MenuItem>
+                            <MenuItem>Sunday</MenuItem>
+                        </DropdownButton>
+                    </Row>
+                    <Row>
+                        <Panel>
+                            <ListGroup>
+                                <EventElement />
+                                <EventElement />
+                                <EventElement />
+                                <EventElement />
+                            </ListGroup>
+                        </Panel>
+                    </Row>
+                </Row>
             </Panel>
        )
     }
--- react-app/app/containers/HomePage.js
+++ react-app/app/containers/HomePage.js
@@ -1,13 +1,26 @@
 // @flow
 import React, { Component } from 'react';
-import EventElement from '../components/EventElement/EventElement.jsx';
+import {Grid, Row, Col} from 'react-bootstrap';
+import EventList from '../components/EventList/EventList.jsx';
+import Header from '../components/Header/Header.jsx';
+import Navbar from '../components/Navbar/Navbar.js';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
-          <EventElement />
-      </div>
+        <Grid>
+            <Row>
+                <Header />
+            </Row>
+            <Row>
+                <Col sm={2} md={2}>
+                    <Navbar />
+                </Col>
+                <Col sm={7} md={7} smOffset={2} mdOffset={2}>
+                    <EventList />
+                </Col>
+            </Row>
+        </Grid>
     );
   }
 }
