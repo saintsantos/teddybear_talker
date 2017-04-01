b32ea97abf44603b6b7d48ac89e1828fc9ba93b7
Edwin Santos <saintsantos1341@gmail.com>
2017-04-01 11:15:28 -0400

    working on grabbing entire week

--- server/app/controllers/calendar.controller.js
+++ server/app/controllers/calendar.controller.js
@@ -1,26 +1,13 @@
 import db from '../config/db';
 function update(req, res, next) {
-    //db(req.params.day).where({'hour': req.params.hour, 'minutes': req.params.minutes}).update({'file_id': req.query.file_id});
-    let obj = {};
-    obj.day = req.params.day;
-    obj.hour = req.params.hour;
-    obj.minutes = req.params.minutes;
-    obj.file = req.query.file_id;
+    db(req.params.day).where({'hour': req.params.hour, 'minutes': req.params.minutes}).update({'file_id': req.query.file_id});
 
-    res.send(obj);
 }
 
 function addEvent(req, res, next) {
-    db('monday').insert({hour: req.params.hour, minutes: req.params.minutes, file_id: req.query.file_id}).then(function() {
+    db(req.params.day).insert({hour: req.params.hour, minutes: req.params.minutes, file_id: req.query.file_id}).then(function() {
         res.status(200).send();
     });
-    /*let obj = {};
-    obj.day = req.params.day;
-    obj.hour = req.params.hour;
-    obj.minutes = req.params.minutes;
-    obj.file = req.query.file_id;
-
-    res.send(obj);*/
 }
 
 function getEvent(req, res, next) {
@@ -30,14 +17,26 @@ function getEvent(req, res, next) {
 }
 
 function getDay(req, res, next) {
-    db.select().table('test').then(function(result) {
+    db(req.params.day).select().table('test').then(function(result) {
         res.send('Grab an entire day');
     });
 }
 
 function getWeek(req, res, next) {
-    db.select().table('test').then(function(result) {
-        res.send('Grab an entire week');
+    db
+    .select()
+    .from('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
+    .then(function(monday, tuesday, wednesday, thursday, friday, saturday, sunday) {
+        let obj = {};
+        obj.monday = monday;
+        obj.tuesday = tuesday;
+        obj.wednesday = wednesday;
+        obj.thursday = thursday;
+        obj.friday = friday;
+        obj.saturday = saturday;
+        obj.sunday = sunday;
+        res.send(obj);
+        console.log(obj);
     });
 }
 
