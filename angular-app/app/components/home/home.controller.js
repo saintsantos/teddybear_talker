5596ea61bf8f8c6c804ba494e31e2e7b7d476795
Edwin Santos <saintsantos1341@gmail.com>
2017-04-04 19:58:58 -0400

    Added floating button for uploads

--- angular-app/app/components/home/home.controller.js
+++ angular-app/app/components/home/home.controller.js
@@ -24,23 +24,23 @@
         }
         $scope.goToUpload = function() {
             $state.go('upload');
-        }	
+        }
     }
 
-		function Weekday_Dropdown($scope) {  
-				$scope.values = [  
-					{'Dropdown': 'D1'},  
-        ];  
-        $scope.colorcode = [  
+		function Weekday_Dropdown($scope) {
+				$scope.values = [
+					{'Dropdown': 'D1'},
+        ];
+        $scope.colorcode = [
 						{'hashCode': 'Su', 'description': 'Sunday'},
 						{'hashCode': 'Mo', 'description': 'Monday'},  
-            {'hashCode': 'Tu', 'description': 'Tuesday'},  
-            {'hashCode': 'We', 'description': 'Wednesday'},  
-            {'hashCode': 'Th', 'description': 'Thursday'}, 
+            {'hashCode': 'Tu', 'description': 'Tuesday'},
+            {'hashCode': 'We', 'description': 'Wednesday'},
+            {'hashCode': 'Th', 'description': 'Thursday'},
 						{'hashCode': 'Fr', 'description': 'Friday'},
 						{'hashCode': 'Sa', 'description': 'Saturday'}
-        ];  
-    }  
+        ];
+    }
 
   angular
     .module('home.controller', ['ui.materialize'])
