001a2c23f7822cd43e22313d41481cdf658f859f
Edwin Santos <saintsantos1341@gmail.com>
2017-04-04 19:32:26 -0400

    Added day and week card

--- angular-app/app/components/home/home.controller.js
+++ angular-app/app/components/home/home.controller.js
@@ -1,6 +1,20 @@
 ;(function() {
 
     function HomeController($scope, $state) {
+
+        $scope.events = [];
+        var event1 = {}
+        event1.hour = 10;
+        event1.min = 5;
+        event1.file = "file1";
+        $scope.events.push(event1);
+
+        var event2 = {}
+        event2.hour = 9;
+        event2.min = 30;
+        event2.file = "file2";
+        $scope.events.push(event2);
+
         $scope.goToHome = function() {
             $state.go('home');
         }
