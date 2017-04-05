58139714106e8eca7cab946d1aa7c5cdcdc44c83
Edwin Santos <saintsantos1341@gmail.com>
2017-04-04 22:55:44 -0400

    Added calendar http requests

--- angular-app/app/components/home/home.controller.js
+++ angular-app/app/components/home/home.controller.js
@@ -30,21 +30,6 @@
         }
     }
 
-		function Weekday_Dropdown($scope) {
-				$scope.values = [
-					{'Dropdown': 'D1'},
-        ];
-        $scope.colorcode = [
-						{'hashCode': 'Su', 'description': 'Sunday'},
-						{'hashCode': 'Mo', 'description': 'Monday'},
-            {'hashCode': 'Tu', 'description': 'Tuesday'},
-            {'hashCode': 'We', 'description': 'Wednesday'},
-            {'hashCode': 'Th', 'description': 'Thursday'},
-						{'hashCode': 'Fr', 'description': 'Friday'},
-						{'hashCode': 'Sa', 'description': 'Saturday'}
-        ];
-    }
-
   angular
     .module('home.controller', ['ui.materialize'])
     .controller('HomeController', HomeController);
--- angular-app/app/components/home/home.service.js
+++ angular-app/app/components/home/home.service.js
@@ -1,5 +1,67 @@
 ;(function() {
 
   angular
-    .module('home.service', [])
+    .module('home.service', []),
+    .factory('HomeService', function($http, $window, $q, $urlConstant) {
+
+      function getDay(day) {
+        var xhrParams = {
+          method: 'GET',
+          url: urlConstant.baseUrl + '/api/calendar/' + day + '/'
+        }
+        return $http(xhrParams);
+      }
+
+      function getEvent(event) {
+        var xhrParams = {
+          method: 'GET',
+          url: urlConstant.baseUrl + '/api/calendar/' + event.day + '/' + event.hour '/' + event.min + '/'
+        }
+        return $http(xhrParams);
+      }
+
+      function getWeek() {
+        var xhrParams = {
+          method: 'GET',
+          url: urlConstant.baseUrl + '/api/calendar/'
+        }
+        return $http(xhrParams);
+      }
+
+      function updateEvent(event) {
+        var xhrParams = {
+          method: 'PUT',
+          url: urlConstant.baseUrl + '/api/calendar/' + event.day + '/' + event.hour '/' + event.min + '/'
+        }
+      }
+
+      function deleteEvent(event) {
+        var xhrParams = {
+          method: 'DELETE',
+          url: urlConstant.baseUrl + '/api/calendar/' + event.day + '/' + event.hour '/' + event.min + '/'
+        }
+        return $http(xhrParams);
+      }
+
+      function addEvent(event) {
+        var xhrParams = {
+          method: 'POST',
+          url: urlConstant.baseUrl + '/api/calendar/' + event.day + '/' + event.hour '/' + event.min + '/'
+          params: {
+            file_id: event.file_id
+          }
+
+        }
+        return $http(xhrParams);
+      }
+
+      return {
+        getDay: getDay,
+        getEvent: getEvent,
+        getWeek: getWeek,
+        updateEvent: updateEvent,
+        deleteEvent: deleteEvent,
+        addEvent: addEvent
+      }
+    })
 })();
