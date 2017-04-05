1e7cf61d28aac3496efe587810c71769ae78e66b
Edwin Santos <saintsantos1341@gmail.com>
2017-04-04 21:18:36 -0400

    Added clock picker for event entry

--- angular-app/app/components/files/file.controller.js
+++ angular-app/app/components/files/file.controller.js
@@ -1,12 +1,24 @@
 ;(function() {
 
     function FileController($scope, $state) {
-        $scope.goToHome = function() {
-            $state.go('home');
-        }
-        $scope.goToSettings = function() {
-            $state.go('settings');
-        }
+
+      $scope.sounds = [];
+      var sound1 = {}
+      sound1.path = "~/Music/test1";
+      sound1.name = "Yay!";
+      $scope.sounds.push(sound1);
+
+      var sound2 = {}
+      sound2.path = "~/Music/test2";
+      sound2.name = "Hello!";
+      $scope.sounds.push(sound2);
+
+      $scope.goToHome = function() {
+          $state.go('home');
+      }
+      $scope.goToSettings = function() {
+          $state.go('settings');
+      }
     }
 
   angular
