f65b538fd4665d3693274f91829415907bdb9003
Edwin Santos <saintsantos1341@gmail.com>
2017-04-08 13:06:58 -0400

    Added file backend calls

--- server-php/src/app/files.php
+++ server-php/src/app/files.php
@@ -3,7 +3,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->group('/files', function () use ($app) {
+$app->group('/voice', function () use ($app) {
 
   //public function getUsers(Request $request, Response $response) {
   $app->get('/', function(Request $request, Response $response) {
@@ -27,6 +27,9 @@ $app->group('/files', function () use ($app) {
 
   // Receive file upload
   $app->post('/upload', function(Request $request, Response $response) {
+    //TODO - Check if the filename exists in the audio table
+    //TODO - Reactivate the index at the proper location in
+    //TODO - the file table and update.
     $files = $request->getUploadedFiles();
     if (empty($files['audio'])) {
       throw new Exception('Expected an audio file');
@@ -43,17 +46,45 @@ $app->group('/files', function () use ($app) {
 
   // Update a file in our table
   $app->put('/{id}', function(Request $request, Response $response) {
+    $this->logger->addInfo("Updating audio file");
+    $id = $request->getAttribute('route')->getArgument('id');
+    //$filename = $request->getUri()->getQuery();
+    $body = $request->getParsedBody();
+    $newname = $body["name"];
+    $uploads_dir = '/home/edwin/Music/uploads';
+    $file = $this->db->query("SELECT * from audio where id=$id");
+    print_r($newname);
+    $name = NULL;
+    $filepath = NULL;
+    foreach( $file as $row) {
+      $name = $row["audio_name"];
+    }
 
+    rename("$uploads_dir/$name.mp3", "$uploads_dir/$newname.mp3");
+    $this->db->query("UPDATE audio set filepath='$uploads_dir/$newname.mp3', audio_name='$newname' where id=$id");
   });
 
   // Delete a recording from the bear
   $app->delete('/{id}', function(Request $request, Response $response) {
-
+    $this->logger->addInfo("Updating deactivating an audio file");
+    $id = $request->getAttribute('route')->getArgument('id');
+    $uploads_dir = '/home/edwin/Music/uploads';
+    $file = $this->db->query("SELECT * from audio where id=$id");
+    print_r($newname);
+    $name = NULL;
+    $filepath = NULL;
+    foreach( $file as $row) {
+      $name = $row["audio_name"];
+    }
+    unlink("$uploads_dir/$name.mp3");
+    $this->db->query("UPDATE audio set status='inactive' where id=$id");
   });
 
   // Delete all recordings from the bear
   $app->delete('/', function(Request $request, Response $response) {
-
+    $uploads_dir = '/home/edwin/Music/uploads';
+    unlink("$uploads_dir/*.mp3");
+    $this->db->query("UPDATE audio set status='inactive'");
   });
 
 });
