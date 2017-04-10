75061b1fe5a43df3fe2be02737188ab24ac1f7ea
Edwin Santos <saintsantos1341@gmail.com>
2017-04-10 14:17:27 -0400

    Added a database dump

--- database/bear.sql
+++ database/bear.sql
@@ -31,12 +31,12 @@ DROP TABLE IF EXISTS `audio`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `audio` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `audio_id` int(11) NOT NULL,
   `audio_name` text COLLATE utf8mb4_unicode_ci,
   `filepath` text COLLATE utf8mb4_unicode_ci,
   `status` text COLLATE utf8mb4_unicode_ci,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`audio_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `audio` (
 
 LOCK TABLES `audio` WRITE;
 /*!40000 ALTER TABLE `audio` DISABLE KEYS */;
-INSERT INTO `audio` VALUES (1,'test_audio','~/audio/test','active');
+INSERT INTO `audio` VALUES (1,'test_audio','/home/pi/test_audio.mp3','active'),(2,'testingaudio1','/home/pi/testingaudio1.mp3','active'),(3,'test1','/home/pi/test1.mp3','active'),(4,'clint eastwood','/home/pi/clint_eastwood.mp3','active'),(5,'melacholy hill','/home/pi/melancholy_hill.mp3','active'),(6,'all alone','/home/pi/all_alone.mp3','active'),(7,'testingaudio2','/home/pi/testingaudio2.mp3','active'),(8,'beautiful and tragic','/home/pi/beautiful_and_tragic.mp3','active'),(9,'testify','/home/pi/testify.mp3','active');
 /*!40000 ALTER TABLE `audio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,13 +58,14 @@ DROP TABLE IF EXISTS `events`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `hour` int(11) DEFAULT NULL,
-  `min` int(11) DEFAULT NULL,
+  `timeDay` text COLLATE utf8mb4_unicode_ci,
   `file_id` int(11) DEFAULT NULL,
   `day` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`),
+  KEY `file_id` (`file_id`),
+  CONSTRAINT `events_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `audio` (`audio_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +74,7 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
-INSERT INTO `events` VALUES (1,8,30,1,NULL,'inactive'),(2,2,30,2,NULL,'inactive'),(3,2,30,2,'monday','active'),(5,11,12,2,'wednesday','active'),(6,18,48,2,'thursday','active'),(7,17,11,2,'friday','active'),(8,2,3,2,'saturday','active'),(9,6,11,2,'sunday','active'),(10,4,6,6,'tuesday','active'),(16,3,25,7,'tuesday','active'),(17,3,25,7,'monday','active'),(18,3,25,7,'wednesday','active'),(19,3,25,7,'thursday','active'),(20,3,25,7,'friday','active'),(21,3,25,7,'saturday','active'),(22,3,25,7,'sunday','active');
+INSERT INTO `events` VALUES (1,NULL,1,NULL,'inactive'),(2,NULL,2,NULL,'inactive'),(3,'13:30',2,'monday','inactive'),(5,'10:14',9,'wednesday','active'),(6,'01:40',2,'thursday','active'),(7,'01:45',2,'friday','inactive'),(8,'14:23',2,'saturday','active'),(9,'15:18',2,'sunday','active'),(10,'20:10',9,'tuesday','active'),(16,'20:10',7,'tuesday','inactive'),(17,'22:14',7,'monday','inactive'),(18,'23:15',7,'wednesday','active'),(19,'11:15',7,'thursday','active'),(20,'20:03',7,'friday','active'),(21,'11:03',7,'saturday','active'),(22,'13:03',7,'sunday','active'),(23,'09:17',7,'tuesday','active'),(24,'09:10',7,'tuesday','active'),(25,'19:10',9,'wednesday','active'),(26,'22:10',7,'tuesday','inactive'),(27,'11:12',7,'wednesday','active');
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-05 10:36:33
+-- Dump completed on 2017-04-09 13:57:39
