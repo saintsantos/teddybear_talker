58b5f1c4703adcb2a0cf5c02fbc48bde3cd8cd8b
Edwin Santos <saintsantos1341@gmail.com>
2017-04-25 21:33:04 -0400

    Added screen script and a couple missing bower dependencies

---
+++ scripts/frontend.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cd ~/git/school/cse453/teddy_bear_talker/angular-app && yarn start
---
+++ scripts/launch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+screen -d -m -s angular ./frontend.sh
+screen -d -m -s php ./backend.sh
