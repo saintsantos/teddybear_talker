51fc07171add92bd8ce3042577a0449e15d148c8
Edwin Santos <saintsantos1341@gmail.com>
2017-05-02 17:09:11 -0400

    Updated access script, added time update script

--- scripts/audio.py
+++ scripts/audio.py
@@ -6,11 +6,11 @@ conn = sqlite3.connect('/home/edwin/git/school/cse453/teddy_bear_talker/database
 c = conn.cursor()
 c.execute("Select * from active")
 result = c.fetchone()
-print result
+#print result
 event = result[0]
 c.execute("SELECT * from events inner join voice on voice.voice_id=events.voice_id inner join jingle on jingle.jingle_id=events.jingle_id where events.event_id=%d" % event)
 result = c.fetchone()
-print result
+#print result
 voice_path = result[8]
 jingle_path = result[12]
 subprocess.Popen(['mpg123', '-q', jingle_path]).wait()
