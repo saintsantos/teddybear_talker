9365afc7ddc05be28f0e6c751776736a67ee5c0d
Edwin Santos <saintsantos1341@gmail.com>
2017-05-05 23:28:49 -0400

    Added date call to frontend

--- angular-app/app/components/settings/settings.controller.js
+++ angular-app/app/components/settings/settings.controller.js
@@ -1,6 +1,6 @@
 ;(function() {
 
-    function SettingsController($scope, $state) {
+    function SettingsController($scope, $state, SystemService) {
         $scope.goToHome = function() {
             $state.go('home');
         }
@@ -9,6 +9,22 @@
         }
         $scope.goToJingle = function() {
             $state.go('jingle');
+        }
+
+        $scope.sendDate = function() {
+          dateTime = {}
+          months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+          var date = new Date();
+          currentMonth = date.getMonth();
+
+          dateTime.year = date.getFullYear();
+          dateTime.month = months[currentMonth];
+          dateTime.day = date.getDate();
+          dateTime.hour = date.getHours();
+          dateTime.minute = date.getMinutes();
+          console.log(dateTime);
+          SystemService.sendDate(dateTime);
+
         }
     }
 
--- angular-app/app/components/settings/settings.service.js
+++ angular-app/app/components/settings/settings.service.js
@@ -2,4 +2,31 @@
 
   angular
     .module('settings.service', [])
+    .factory('SystemService', function($http, $window, $q, urlConstant) {
+
+      function reboot() {
+        var xhrParams = {
+          method: 'POST',
+          url: urlConstant.baseUrl + '/system/reboot'
+        }
+        return $http(xhrParams);
+      }
+
+      function sendDate(dateTime) {
+        var xhrParams = {
+          method: 'POST',
+          url: urlConstant.baseUrl + '/system/date',
+          data: dateTime
+        }
+        return $http(xhrParams);
+      }
+
+      return {
+        reboot: reboot,
+        sendDate: sendDate
+      }
+
+    });
+
+
 })();
