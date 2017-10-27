7e21fd683cffb1d249ce7a1da5e5692e51008a9a
saintsantos <saintsantos1341@gmail.com>
2017-10-27 17:23:02 -0400

    Add comments to python server

--- server/python/app.py
+++ server/python/app.py
@@ -6,9 +6,11 @@ from flask_cors import CORS
 from subprocess import call
 import os
 
+# Allowed file extensions and the directory to upload our audio files to (changes on the raspberry pi)
 ALLOWED_EXTENSIONS = set(['mp3', 'wma', 'wav', 'm4a'])
 UPLOAD_DIR = '/Users/edwinsantos/Music/audio/'
 
+# Globals for our API
 app = Flask(__name__, static_folder='./static/build', template_folder='./static')
 app.config['DEBUG'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
@@ -19,6 +21,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
+# Check our list of allowed files to see if our uploaded file is acceptable
 def allowed_file(filename):
     # check if our filetype is allowed
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -50,13 +53,14 @@ class Events(db.Model):
         self.music = music
         self.day = day
 
+# This table stores the currently active event in the application
 class Active(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     event_id = db.Column(db.Integer)
 
 db.create_all()
 
-
+# These are used for validation and converting the python data (dict) to json
 class EventSchema(ma.ModelSchema):
     class Meta:
         model = Events
@@ -70,8 +74,10 @@ class AudioSchema(ma.ModelSchema):
 audio_schema = AudioSchema()
 audios_schema = AudioSchema(many=True)
 
+# Event endpoints
 @app.route('/api/events/<day>', methods=['GET'])
 def get_day(day):
+    # Returns an array of events specified by the <day> string sent to the endpoint
     # print(day)
     return jsonify(events=events_schema.dump(Events.query.filter_by(day=day)).data)
 
@@ -79,6 +85,7 @@ def get_day(day):
 @app.route('/api/events/<id>', methods=['DELETE', 'PATCH'])
 def update_event(id):
     if request.method == 'PATCH':
+        # Update an event
         update_data = request.get_json()
         errors = event_schema.validate(update_data, partial=True)
         if errors:
@@ -87,6 +94,7 @@ def update_event(id):
         db.session.commit()
         return jsonify(event_schema.dump(event).data), 200, {'ContentType': 'application/json'}
     else:
+        # Delete an event
         Events.query.filter_by(id=id).delete()
         db.session.commit()
         return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
@@ -95,6 +103,7 @@ def update_event(id):
 # TODO - Check if an event already exists for the bear before adding a new event
 @app.route('/api/events/', methods=['POST'])
 def create_events():
+    # Create a new event
     event_data = request.get_json()
     print(event_data)
     error = event_schema.validate(event_data, partial=True)
@@ -111,9 +120,11 @@ def create_events():
     return jsonify(event_schema.dump(event).data)
 
 
+# Audio endpoints
 @app.route('/api/audio/<id>', methods=['DELETE', 'PATCH'])
 def modify_audio(id):
     if request.method == 'PATCH':
+        # Change the name of the audio file (only on db for display. Not actual file)
         update_data = request.get_json()
         error = audio_schema.validate(update_data, partial=True)
         if error:
@@ -122,6 +133,9 @@ def modify_audio(id):
         db.session.commit()
         return jsonify(audio_schema.dump(audio).data), 200, {'ContentType': 'application/json'}
     else:
+        # Delete an audio file from both the db and the actual filesystem
+        audio = Audio.query.get(id)
+        os.remove(audio.path)
         Audio.query.filter_by(id=id).delete()
         db.session.commit()
         return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
@@ -129,13 +143,14 @@ def modify_audio(id):
 
 @app.route('/api/audio/<form>')
 def filter_audio(form):
+    # Experimental filtering by audio type
     return jsonify(audio=audios_schema.dump(Audio.query.filter_by(form=form)).data)
 
 
 @app.route('/api/audio/', methods=['GET', 'POST'])
 def audio_handler():
-    # TODO - Add capabilities to rename files when they are uploaded.
     if request.method == 'POST':
+        # Add a new audio file to the db
         if 'file' not in request.files:
             return jsonify({'No file': 'No file uploaded'}), 400
         file = request.files['file']
@@ -155,23 +170,28 @@ def audio_handler():
         else:
             return jsonify({'File Not Allowed': 'This file is not allowed'}), 400
     else:
+        # Get the list of all audio files on the bear
         return jsonify(audio=audios_schema.dump(Audio.query.all()).data)
 
 
+# System calls for the bear
 @app.route('/api/reboot', methods=['POST'])
 def reboot():
+    # Reboot the bear
     call(["echo", "Make the bear reboot"])
     return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/api/date', methods=['POST'])
 def date_update():
+    # Update the date and time of the bear
     call(["echo", "Update the date and time of the bear"])
     return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/api/clean', methods=['POST'])
 def clean():
+    # Drop all the tables and recreate for a clean installation
     db.drop_all()
     db.create_all()
     call(["echo", "Reset the bear"])
@@ -187,6 +207,7 @@ def clean():
 
 @app.route('/api/test/audio/<id>', methods=['POST'])
 def test_audio(id):
+    # Test an audio file to see how it sounds
     audio = Audio.query.get(id)
     call(["mplayer", audio.path])
     return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
@@ -194,6 +215,7 @@ def test_audio(id):
 
 @app.route('/api/test/event/<id>', methods=['POST'])
 def test_event(id):
+    # Test an event functions properly
     event = Events.query.get(id)
     voice = Audio.query.get(event.voice)
     music = Audio.query.get(event.music)
@@ -201,10 +223,12 @@ def test_event(id):
     call(["mplayer", music.path])
     return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
 
+# This endpoint is catch all for any urls in our react app. Allows for proper app refreshes
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template('react.jinja2')
 
+# Actually start our app
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
