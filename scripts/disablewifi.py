039b4ff1be136e308f320fab52314ad8cac025b5
saintsantos <saintsantos1341@gmail.com>
2017-11-05 17:45:50 -0500

    Remove more legacy commands

--- scripts/disablewifi.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python2.7
-import subprocess
-
-subprocess.call(['sudo', 'ifdown', 'wlan0'])
-
--- scripts/teardown.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env python2.7
-import subprocess
-subprocess.call(['screen', '-X', '-S', 'angular', 'quit'])
-subprocess.call(['screen', '-X', '-S', 'php', 'quit'])
