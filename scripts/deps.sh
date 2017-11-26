9f1b410a379208629691d3d07766365144fc8fe9
saintsantos <saintsantos1341@gmail.com>
2017-11-26 11:24:17 -0500

    Add yes argument to installer script

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,7 +3,7 @@
 echo "This script will deploy all dependencies of teddybear talker"
 
 sudo apt update
-sudo apt install python3-dev python3-venv nginx hostapd dnsmasq device-tree-compiler git omxplayer vim
+sudo apt install -y python3-dev python3-venv nginx hostapd dnsmasq device-tree-compiler git omxplayer vim
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 mkdir ~/audio
 git clone https://github.com/saintsantos/teddybear_talker.git
