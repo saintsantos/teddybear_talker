eb48141c4e024dc97ac93f85398a54be4dfa072f
saintsantos <saintsantos1341@gmail.com>
2017-11-26 10:20:22 -0500

    Add clone command to deps.sh

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -6,3 +6,4 @@ sudo apt update
 sudo apt install python3-dev python3-venv nginx hostapd dnsmasq device-tree-compiler git omxplayer vim
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 mkdir ~/audio
+git clone https://github.com/saintsantos/teddybear_talker.git
