de3306ef0ae6ae02814a5e9e6c20d31c843664ff
saintsantos <saintsantos1341@gmail.com>
2017-11-26 09:49:03 -0500

    Add mxplayer and vim

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,6 +3,6 @@
 echo "This script will deploy all dependencies of teddybear talker"
 
 sudo apt update
-sudo apt install python3-dev python3-venv nginx hostapd dnsmasq device-tree-compiler git
+sudo apt install python3-dev python3-venv nginx hostapd dnsmasq device-tree-compiler git omxplayer vim
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 mkdir ~/audio
