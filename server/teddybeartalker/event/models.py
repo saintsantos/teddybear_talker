0a6010f0a121843ab66f659b39e927179fb334b2
saintsantos <santos.r.edwin@gmail.com>
2019-12-11 13:33:19 -0500

    Convert day field to integer selection in the DB

--- server/teddybeartalker/event/models.py
+++ server/teddybeartalker/event/models.py
@@ -7,7 +7,16 @@ class Event(models.Model):
     time = models.TimeField()
     voice = models.ForeignKey(Audio, blank=True, null=True, on_delete=models.SET_NULL, related_name="voice")
     music = models.ForeignKey(Audio, blank=True, null=True, on_delete=models.SET_NULL, related_name="music")
-    day = models.CharField(max_length=10)
+    day = models.IntegerField(
+        choices=[
+            (0, "Monday"),
+            (1, "Tuesday"),
+            (2,"Wednesday"),
+            (3, "Thursday"),
+            (4, "Friday"),
+            (5, "Saturday"),
+            (6, "Sunday")
+        ])
 
 class Active(models.Model):
     event_id = models.ForeignKey(Event, blank=True, null=True, on_delete=models.SET_NULL)
